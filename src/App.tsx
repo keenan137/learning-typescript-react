c42dcf032f44ff10ac6a4c7232d94d78c5e2b435
keenan137 <keenan.dup137@gmail.com>
2023-12-10 16:19:06 +0200

    Extracted anonymous method into a function.

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,15 @@ import './App.css'
 function App(): JSX.Element {
   const [count, setCount] = useState<number>(0)
 
+  const incrementCounter = (): void =>{
+    setCount(count => count + 1);
+  }
+
   return (
     <>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => incrementCounter()}>
           count is {count}
         </button>
       </div>
